eb0ecaa2aa43113ad65498a0f207ef8a27d9a8ba
Alex Johnson <alexjohnson@Alexs-M2-Macbook-Pro.local>
2024-12-01 17:16:58 -0600

    chore: set up arrays for solution

--- day-one/solution.py
+++ day-one/solution.py
@@ -1,3 +1,15 @@
 class solution:
-    def add(self, a, b):
-        return a + b
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def solve(self):
+        return self.a[0] + self.b[0]
+
+    def get_arrays(self, file_name):
+        with open(file_name, "r") as file:
+            for line in file:
+                x, y = map(int, line.split())
+
+                self.a.append(x)
+                self.b.append(y)
